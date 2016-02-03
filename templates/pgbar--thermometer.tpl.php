f6fce5067d250a675b34918036971edeee584dae
Roman Zimmermann <roman@more-onion.com>
2016-02-03 16:48:31 +0100

    [pgbar] Template: Add html-id to output.

--- templates/pgbar--thermometer.tpl.php
+++ templates/pgbar--thermometer.tpl.php
@@ -11,7 +11,7 @@ $vars['!needed'] = number_format($target - $current, 0);
 $intro_message  = t($goal_reached ? $texts['full_intro_message']  : $texts['intro_message'], $vars);
 $status_message = t($goal_reached ? $texts['full_status_message'] : $texts['status_message'], $vars) . "\n";
 ?>
-<div class="pgbar-wrapper clearfix pgbar-wrapper-thermometer" data-pgbar-current="<?php print $current; ?>" data-pgbar-target="<?php print $target; ?>" data-pgbar-inverted="false" data-pgbar-direction="vertical">
+<div id="<?php print $html_id; ?>" class="pgbar-wrapper clearfix pgbar-wrapper-thermometer" data-pgbar-current="<?php print $current; ?>" data-pgbar-target="<?php print $target; ?>" data-pgbar-inverted="false" data-pgbar-direction="vertical">
   <p><?php print $status_message; ?></p> 
   <p><?php print $intro_message; ?></p>
   <div class="thermometer">
