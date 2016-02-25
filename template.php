1de03bec6e04adb75703b0e7b944e410b54a863f
Katharina Zwinger <katharina@more-onion.com>
2016-02-25 16:10:03 +0100

    Allow subthemes to override files imported by less

--- template.php
+++ template.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Implements HOOK_preprocess_field.
+ * Implements hook_preprocess_field().
  *
  * Overrides pgbar settings for teasers to
  * always display pgbar as progress bar.
@@ -12,5 +12,29 @@ function simplicity_preprocess_field(&$vars) {
   if (   $element['#field_type'] == 'pgbar'
       && in_array($element['#view_mode'], array('teaser'))) {
     $vars['items'][0]['#theme'][0] = 'pgbar__pbgar';
+  }
+}
+
+/**
+ * Implements hook_less_paths_alter().
+ *
+ * Adds the path of the current theme and all
+ * its base themes to the less import paths.
+ *
+ * If a file imported in a base theme's less file does
+ * not exist, the less compiler looks for it in the current
+ * theme, then the current theme's base theme, and so on.
+ * Using paths relative to the theme instead of the less
+ * file allows subthemes to override imported files.
+ */
+function simplicity_less_paths_alter(&$less_paths, $system_name){
+  $themes = $GLOBALS['theme_info']->base_themes;
+  foreach (array_keys($themes) as $theme){
+    $themes[$theme] = DRUPAL_ROOT . '/' . drupal_get_path('theme', $theme);
+  }
+  $themes[$GLOBALS['theme']] =  DRUPAL_ROOT . '/' . $GLOBALS['theme_path'];
+
+  if (isset($themes[$system_name])) {
+    $less_paths += array_reverse(array_values($themes));
   }
 }
