7deab3f30036b475b5467d02b64fc301383e5cbe
Katharina Zwinger <katharina@more-onion.com>
2016-03-02 18:02:06 +0100

    New styling for thermometer (parameterized)

--- templates/pgbar--thermometer.tpl.php
+++ templates/pgbar--thermometer.tpl.php
@@ -11,15 +11,16 @@ $vars['!needed'] = number_format($target - $current, 0);
 $intro_message  = t($goal_reached ? $texts['full_intro_message']  : $texts['intro_message'], $vars);
 $status_message = t($goal_reached ? $texts['full_status_message'] : $texts['status_message'], $vars) . "\n";
 ?>
-<div class="pgbar-wrapper clearfix pgbar-wrapper-thermometer" data-pgbar-current="<?php print $current; ?>" data-pgbar-target="<?php print $target; ?>" data-pgbar-inverted="true" data-pgbar-direction="vertical">
+<div class="pgbar-wrapper clearfix pgbar-wrapper-thermometer" data-pgbar-current="<?php print $current; ?>" data-pgbar-target="<?php print $target; ?>" data-pgbar-inverted="false" data-pgbar-direction="vertical">
   <p><?php print $status_message; ?></p> 
   <p><?php print $intro_message; ?></p>
-  <div class="pgbar-bg">
-    <div class="pgbar-maxheight">
+  <div class="thermometer">
+    <div class="bulb"></div>
+    <div class="pgbar-bg">
       <div class="pgbar-current" style="height:<?php echo $percentage; ?>%"> </div>
     </div>
-    <div class="pgbar-overlay" style=""> </div>
+    <div class="thermometer-overlay"> </div>
 
+    <div class="pgbar-percent"><?php print number_format($percentage, 0) . '%'; ?></div>
   </div>
-  <div class="pgbar-percent"><?php print number_format($percentage, 0) . '%'; ?></div>
 </div>
